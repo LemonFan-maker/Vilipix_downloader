123695eb54646b252eb86b3b78624048c2b878af
LemonFan-maker <2254650260@qq.com>
2023-01-06 13:21:21 +0800

    补充"佚名画师"

--- vilipix下崽器_加强版CLI.py
+++ vilipix下崽器_加强版CLI.py
@@ -20,7 +20,61 @@ log_group = parser.add_argument_group("日志")
 log_group.add_argument('-l', "--log", action="store_true", help="日志保存")
 
 def callback(param):
-    return param
+    if re.search('[a-zA-Z]', param):
+        noname(param)
+    else:
+        return param
+
+def noname(param):
+    noname_webpages(param)
+
+def noname_webpages(illust):
+    url = "https://www.vilipix.com/illust/"+str(illust)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
+    }
+    response = requests.get(url, headers=headers).status_code
+    if response in range(200,300):
+        noname_downpic(illust)
+    else:
+        print("报错",response,"请检查illust是否存在.")
+        os._exit(0)
+    
+def noname_downpic(illust):
+    url = "https://www.vilipix.com/illust/"+str(illust)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    data = soup.find_all('ul',attrs={'class':'illust-pages'})
+    imglist = soup.find_all('img')
+    lenth = len(imglist)  #计算集合的个数
+    for i in range(lenth-1):
+        dt = str(imglist[i-1])
+        list1 = dt.split(" ")
+        strlist = list1[1:3]
+        strlist = ','.join(strlist)
+        string = strlist.replace("'",'')
+        result = re.findall(r'src="(.*?)"', string)[0]
+        result = re.split(r'\?.{1,}',result)[0]
+        pattern = "<img alt.{1,}/>"
+        element = re.search(pattern, str(data)).group()
+        tree = etree.HTML(element)
+        alt = tree.xpath('//img/@alt')[0]
+        #print("准备下载",alt)
+        noname_save(result, alt)
+    
+def noname_save(url, alt):
+    abs = sys.path[0]
+    downdir = abs+"\\images\\"
+    if os.path.exists(downdir):
+        print('目录存在,忽略')
+    else:    
+        print("目录不存在,创建.")
+        os.makedirs("images")
+    os.chdir(downdir)
+    urlretrieve(url, str(alt)+'.png')
 
 def webpages(illust):
     url = "https://www.vilipix.com/illust/"+str(illust)
@@ -42,22 +96,11 @@ def downpic(illust):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     data = soup.find_all('ul',attrs={'class':'illust-pages'})
-    imglist = soup.find_all('img')
-    lenth = len(imglist)  #计算集合的个数
-    for i in range(lenth-1):
-        dt = str(imglist[i-1])
-        list1 = dt.split(" ")
-        strlist = list1[1:3]
-        strlist = ','.join(strlist)
-        string = strlist.replace("'",'')
-        result = re.findall(r'src="(.*?)"', string)[0]
-        result = re.split(r'\?.{1,}',result)[0]
-        print(result)
-
     for one in data:
         img_url = re.findall("https://img9.vilipix.com/picture/pages/regular/(.*?)_p0_master1200.jpg?",str(one))
         img_date = re.sub('[\[\]\'\"]', '', str(img_url))
-        real_url = "https://img9.vilipix.com/picture/pages/regular/"+str(img_date)+"_p0_master1200.jpg"
+        print(img_date)
+        real_url = "https://img9.vilipix.com/picture/pages/original/"+str(img_date)+"_p0.jpg"
         pattern = "<img alt.{1,}/>"
         element = re.search(pattern, str(data)).group()
         tree = etree.HTML(element)
@@ -81,5 +124,5 @@ param = parser.parse_args().illust
 data = param[0]
 illust = callback(data)
 webpages(data)
-real_url, alt = downpic(data)
-save(real_url,alt,illust)
+result, alt = downpic(data)
+save(result,alt,illust)
